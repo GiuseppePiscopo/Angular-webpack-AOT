e0c1eb49e825654c8ab65574b3b3e42ac695d459
GiuseppePiscopo <GiuseppePiscopo@users.noreply.github.com>
2017-05-24 18:39:57 +0200

    refactor(about): bring back HTML template in file, to show the alternative

--- src/app/components/about/about.component.ts
+++ src/app/components/about/about.component.ts
@@ -2,7 +2,11 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
     selector: 'about',
-    templateUrl: 'about.component.html',
+    template: `
+        <h2>about page</h2>
+        <search *ngIf="true"></search>
+        <p *ngIf="true">about page</p>
+    `
 })
 export class AboutComponent implements OnInit {
     constructor() { }
